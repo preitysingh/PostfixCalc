a765ddcb120ecd85b6929401966fc552a5fdf2ee
keira-kastelic <79609952+keira-kastelic@users.noreply.github.com>
2022-12-01 11:21:50 -0700

    Creating a class testing file

--- PostfixCalc/src/PostfixCalc.java
+++ PostfixCalc/src/PostfixCalc.java
@@ -1,30 +1,20 @@
 
 public class PostfixCalc <E> {
-    private String user = "";
+    private String user = "348*+7+";
     private Stack<Integer> postStack = new Stack<>();
     private int answer;
     private String error = null;
 
-    public void PostFix(){
-
+    public void PostfixCalc(){
     }
 
-    public void PostfixCalc(String user) {
-        this.user = user;
-    }
-
-    public void calculate(String user) {
+    public void calculate() {
         String[] use = user.split("", -1);
         for (String test : use) {
             try {
-                user = user + postStack.push(Integer.parseInt(test));
+                postStack.push(Integer.parseInt(test));
             } catch (NumberFormatException e) {
-                if (!postStack.isEmpty() || postStack.getTop().getChild() == null) {
-                    System.out.println(postStack);
-                    Node<Integer> curr = postStack.getTop();
-                    while (!(curr.getChild() == null)){
-                        System.out.println(curr.getChild());
-                    }
+                if (!(postStack.getTop().getChild() == null || postStack.isEmpty())){
                     int first = postStack.pop();
                     int second = postStack.pop();
                     switch (test) {
@@ -57,7 +47,7 @@ public class PostfixCalc <E> {
         answer = postStack.peek();
     }
 
-    public void setUser() { // setting the user string;
+    public void setUser(String user) { // setting the user string;
         this.user = user;
     }
 
--- PostfixCalc/src/Tester.java
+++ PostfixCalc/src/Tester.java
@@ -2,8 +2,8 @@ public class Tester {
     public static void main(String[] args) {
         String s = "348*+7+";
         PostfixCalc postfix = new PostfixCalc();
-        postfix.setUser();
-        postfix.calculate(s);
+        postfix.setUser(s);
+        postfix.calculate();
         System.out.println(postfix.getAnswer());
         System.out.println(postfix.getUser());
     }
