9e75f6a16bddff12e4613b800fa9e93526d757a4
preitysingh <111521777+preitysingh@users.noreply.github.com>
2022-12-01 10:02:21 -0700

    working on the test driver

--- PostfixCalc/src/TestDriver.java
+++ PostfixCalc/src/TestDriver.java
@@ -1,6 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
+<<<<<<< Updated upstream
 public class TestDriver {
     public static void main(String[] args) {
         Scanner s = new Scanner("PostfixEquation.rtf");
@@ -27,5 +27,40 @@ public class TestDriver {
         }
         // loop post condition: at eof
         s.close();
+=======
+
+public class TestDriver {
+    public static void main(String[] args) throws IOException {
+        PostfixCalc p = new PostfixCalc();
+        BufferedReader br = new BufferedReader(new FileReader("PostfixEquation.rtf"));
+        String equation = br.readLine();
+        //String result;
+        System.out.println(" Here is the equation before: " + equation);
+        while(equation  != null){
+            p = equation.getAnswer();
+            System.out.println( " Result = " + p);
+        }
+
+
+//        answerLine.write(result);
+//        answerLine.close(); //closes the file
+
+//        Scanner s = new Scanner("PostfixEquation.rtf");
+//        PostfixCalc p = new PostfixCalc();
+//
+//        while (s.hasNext())
+//        {
+//            // read equation line in text file
+//            String postfix = s.next();
+//            System.out.println("   Here is the Postfix expression:\t" + postfix);
+//            //convert to post fix notation
+//            String result = p.getAnswer();
+//            System.out.println("   Result = " + result + "\n");
+//        }
+//        // loop post condition: at eof
+//        s.close();
+
+
+>>>>>>> Stashed changes
     }
 }
