d4a0d5053d194acb3fc1f037ee312a9753a99db3
preitysingh <111521777+preitysingh@users.noreply.github.com>
2022-12-01 10:05:07 -0700

    editing files

--- PostfixCalc/src/TestDriver.java
+++ PostfixCalc/src/TestDriver.java
@@ -1,34 +1,5 @@
 import java.io.*;
 import java.util.Scanner;
-<<<<<<< Updated upstream
-public class TestDriver {
-    public static void main(String[] args) {
-        Scanner s = new Scanner("PostfixEquation.rtf");
-//        String str;
-//        PostfixCalc postfixNotation = new PostfixCalc();
-//
-//        System.out.println("Enter a expression: ");
-//        str = s.nextLine();
-//
-//        postfixNotation = get.str(setUser);
-//        getUser;
-//
-//        System.out.println("postfix evaluation: "+ postfixNotation.answer(str));
-        PostfixCalc p = new PostfixCalc();
-
-        while (s.hasNext()) // while not eof...
-        {
-            // read equation line in text file
-            String postfix = s.nextLine();
-            System.out.println("   Here is the Postfix expression:\t" + postfix);
-            //convert to post fix notation
-            String result = p.answer(postfix);
-            System.out.println("   Result = " + result + "\n");
-        }
-        // loop post condition: at eof
-        s.close();
-=======
-
 public class TestDriver {
     public static void main(String[] args) throws IOException {
         PostfixCalc p = new PostfixCalc();
@@ -40,8 +11,6 @@ public class TestDriver {
             p = equation.getAnswer();
             System.out.println( " Result = " + p);
         }
-
-
 //        answerLine.write(result);
 //        answerLine.close(); //closes the file
 
@@ -60,7 +29,5 @@ public class TestDriver {
 //        // loop post condition: at eof
 //        s.close();
 
-
->>>>>>> Stashed changes
     }
 }
